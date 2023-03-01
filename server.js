952940a885a3fea2b57daa184fb8995edd80c1a4
Vadym Bondarenko <vadem.bondarenko@gmail.com>
2023-03-01 23:29:09 +0200

    add env ignor

--- server.js
+++ server.js
@@ -2,13 +2,12 @@ const app = require("./app");
 
 const mongoose = require("mongoose");
 
-const DB_HOST =
-  "mongodb+srv://vadym:k8017g9uF97X7ZY8@cluster0.e5ydca8.mongodb.net/Contacts_book?retryWrites=true&w=majority";
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000);
+    app.listen(PORT);
 
     console.log("Database connection successful");
   })
